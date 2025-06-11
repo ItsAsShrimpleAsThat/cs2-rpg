42432afe54839c6170c572e3f88e5cea9979415f
ItsAsShrimpleAsThat <andrew.lang@sonomaacademy.org>
2025-06-10 19:50:38 -0700

    moved listener to a new file to make it look like i have super clean code

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using CounterStrike2GSI;
 using CounterStrike2GSI.EventMessages;
+using cs2_rpg.csinterop;
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace cs2_rpg
 {
@@ -11,48 +13,19 @@ namespace cs2_rpg
 
         static void Main(string[] args)
         {
-            using var fs = new FileStream(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            using var sr = new StreamReader(fs, Encoding.UTF8);
+            Thread listenerThread = new Thread(() => ChatListener.ParseMessageFromConsole(logPath, HandleMessage));
+            listenerThread.IsBackground = true;
+            listenerThread.Start();
+            Console.WriteLine("Started Listening to CS2 Chat...");
 
-            fs.Seek(0, SeekOrigin.End);
+            Console.WriteLine("Finished starting. Press Enter to stop cleanly");
+            Console.ReadLine();
+        }
 
-            while (true)
-            {
-                string? line = sr.ReadLine();
-
-                if (line == null)
-                {
-                    Thread.Sleep(100);
-                    continue;
-                }
-
-                // filter any empty lines
-                if (!string.IsNullOrWhiteSpace(line) && !line.Contains('\0'))
-                {
-                    // check if console line is chat message
-                    if (line.Substring(16, 5) == "[ALL]")
-                    {
-                        // there's an invisible unicode U+200E character dividing the username from the message. thanks Pandaptable (nemmy) i stole this from you :3
-                        string[] parts = line.Substring(22, line.Length - 22).Split("\u200e");
-
-                        string username = parts[0];
-                        string message;
-
-                        if (parts[1].Substring(0, 1) == ":")
-                        {
-                            message = parts[1].Substring(2, parts[1].Length - 2);
-                        }
-                        else
-                        {
-                            message = parts[1].Substring(9, parts[1].Length - 9);
-                        }
-
-                        Console.WriteLine("Full Line: " + line);
-                        Console.WriteLine("Username: " + username);
-                        Console.WriteLine("Message: " + message);
-                    }
-                }
-            }
+        static void HandleMessage(ChatMessage message)
+        {
+            Console.WriteLine("Username: " + message.author);
+            Console.WriteLine("Message: " + message.message);
         }
     }
 }
---
+++ csinterop/ChatMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs2_rpg.csinterop
+{
+    public struct ChatMessage
+    {
+        public string author;
+        public string message;
+
+        public ChatMessage(string author, string message)
+        {
+            this.author = author;
+            this.message = message;
+        }
+    }
+}
